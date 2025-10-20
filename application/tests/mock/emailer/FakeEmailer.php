0c24db59620cd9346d33f42e6f46e234d3c43446
briskt <3172830+briskt@users.noreply.github.com>
2025-10-20 12:20:03 +0800

    fix const name (ASSERT_VALID_BROKER_IP_CONFIG)

--- application/tests/mock/emailer/FakeEmailer.php
+++ application/tests/mock/emailer/FakeEmailer.php
@@ -3,7 +3,6 @@
 namespace tests\mock\emailer;
 
 use common\components\Emailer;
-use tests\mock\emailer\FakeEmailServiceClient;
 
 class FakeEmailer extends Emailer
 {
@@ -18,7 +17,7 @@ class FakeEmailer extends Emailer
                 $this->emailServiceConfig['baseUrl'],
                 $this->emailServiceConfig['accessToken'],
                 [
-                    FakeEmailServiceClient::ASSERT_VALID_IP_CONFIG => $this->emailServiceConfig['assertValidIp'],
+                    FakeEmailServiceClient::ASSERT_VALID_BROKER_IP_CONFIG => $this->emailServiceConfig['assertValidIp'],
                     FakeEmailServiceClient::TRUSTED_IPS_CONFIG => $this->emailServiceConfig['validIpRanges'],
                 ]
             );
