addf545de8427fbbb4542aa00beba0fd0fbcc5d1
briskt <3172830+briskt@users.noreply.github.com>
2026-05-07 10:52:39 +0800

    works as-is, but column names don't need to be wrapped in braces

--- application/console/migrations/m260506_000000_remove_user_hide_column.php
+++ application/console/migrations/m260506_000000_remove_user_hide_column.php
@@ -7,7 +7,7 @@ class m260506_000000_remove_user_hide_column extends Migration
     // Use safeUp/safeDown to run migration code within a transaction
     public function safeUp()
     {
-        $this->dropColumn('{{user}}', '{{hide}}');
+        $this->dropColumn('{{user}}', 'hide');
     }
 
     public function safeDown()
