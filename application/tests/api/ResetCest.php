e59c12fe3335a5557bd06f094cf9bd89ba88deec
briskt <3172830+briskt@users.noreply.github.com>
2026-05-19 15:22:28 +0800

    add a test case for authenticated reset validate request

--- application/tests/api/ResetCest.php
+++ application/tests/api/ResetCest.php
@@ -43,21 +43,21 @@ class ResetCest extends BaseCest
 
     public function test800(ApiTester $I)
     {
-        $I->wantTo('check response when making unauthenticated PUT request to validate a reset code');
+        $I->wantTo('check response when making unauthenticated PUT request to validate a reset UUID');
         $I->sendPUT('/reset/11111111111111111111111111111111/validate');
         $I->seeResponseCodeIs(200);
     }
 
     public function test810(ApiTester $I)
     {
-        $I->wantTo('check response when making unauthenticated PUT request to validate an expired reset code');
+        $I->wantTo('check response when making unauthenticated PUT request to validate an expired reset UUID');
         $I->sendPUT('/reset/22222222222222222222222222222222/validate');
         $I->seeResponseCodeIs(404);
     }
 
     public function test820(ApiTester $I)
     {
-        $I->wantTo('check response when making authenticated PUT request to validate a reset code');
+        $I->wantTo('check response when making authenticated PUT request to validate a reset UUID');
         $I->setCookie('access_token', 'user1', parent::getCookieConfig());
         $I->sendPUT('/reset/33333333333333333333333333333333/validate');
         $I->seeResponseCodeIs(200);
@@ -65,7 +65,7 @@ class ResetCest extends BaseCest
 
     public function test830(ApiTester $I)
     {
-        $I->wantTo('check response when making authenticated DELETE request to reset/id/validate');
+        $I->wantTo('check response when making authenticated DELETE request to reset/{uuid}/validate');
         $I->setCookie('access_token', 'user1', parent::getCookieConfig());
         $I->sendDELETE('/reset/44444444444444444444444444444444/validate');
         $I->seeResponseCodeIs(405);
@@ -82,6 +82,14 @@ class ResetCest extends BaseCest
     {
         $I->wantTo('check response when making a POST request to create a reset for an invalid user');
         $I->sendPOST('/reset', ['username' => 'xxxxx']);
+        $I->seeResponseCodeIs(204);
+    }
+
+    public function test93(ApiTester $I)
+    {
+        $I->wantTo('check response when making an authenticated POST request to create a reset');
+        $I->setCookie('access_token', 'user1', parent::getCookieConfig());
+        $I->sendPOST('/reset', ['username' => 'first_last']);
         $I->seeResponseCodeIs(204);
     }
 }
